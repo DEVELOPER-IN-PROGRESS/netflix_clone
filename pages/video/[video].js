69027d610170da0ec7c9c7992fcae9d6601bcba1
iceweasel <johnsjosephk@protonmail.com>
2021-11-22 23:38:08 +0530

    video modal and styling

--- pages/video/[video].js
+++ pages/video/[video].js
@@ -1,9 +1,26 @@
 import { useRouter } from "next/router";
+import Modal from "react-modal";
+import styles from '../../styles/Video.module.css'
+
+Modal.setAppElement('#__next');
 
 const Video = () => {
     const router = useRouter();
     console.log({router});
-    return ( <div>Video Page {router.query.video}</div>);
+    return ( 
+          <div className={styles.container}>
+            Video Page {router.query.video}
+            <Modal
+            isOpen={true}
+            contentLabel="Watch the video"
+            onRequestClose={() => { console.log("back"); router.back()}}
+           className={styles.modal}
+            overlayClassName={styles.overlay}
+             > 
+           <div>Modal body</div>
+             </Modal>
+ 
+            </div>);
 }
 
 export default Video ; 
