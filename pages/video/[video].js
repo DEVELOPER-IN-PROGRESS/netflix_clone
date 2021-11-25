63a982ff570bc75399c8f76d7fe01aa8543bead1
iceweasel <johnsjosephk@protonmail.com>
2021-11-26 00:19:35 +0530

    modal body  styling

--- pages/video/[video].js
+++ pages/video/[video].js
@@ -1,11 +1,24 @@
 import { useRouter } from "next/router";
+import clsx from "clsx";
 import Modal from "react-modal";
 import styles from '../../styles/Video.module.css'
 
 Modal.setAppElement('#__next');
 
-const Video = () => {
+const Video = ({}) => {
     const router = useRouter();
+
+    const video =  {
+      title: 'White Wolf',
+      publishTime:'december-16-1991',
+      description: 'Just some few random description',
+      channelTitle:'Sokovian Diaries',
+      viewCount: 10000 ,
+    } ;
+
+
+     const { title , publishTime , description , channelTitle , viewCount } = video ; 
+
     return ( 
           <div className={styles.container}>
             Video Page {router.query.video}
@@ -26,8 +39,27 @@ const Video = () => {
                 src={`https://www.youtube.com/embed/${router.query.video}?autoplay=0&origin=http://example.com&controls=0&rel=1`}
                 frameborder="0"
               ></iframe>
-             </Modal>
- 
+              <div className={styles.modalBody}>
+                <div className={styles.modalBodyContent}>
+                  <div className={styles.col1}>
+                    <p className={styles.publishTime}>{publishTime}</p>
+                    <p className={styles.title}>{title}</p>
+                    <p className={styles.description}>{description}</p>
+                  </div>
+                  <div className={styles.col2}>
+                    <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                      <span className={styles.textColor}>Cast: </span>
+                      <span className={styles.channelTitle}>{channelTitle}</span>
+                    </p>
+                    <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                      <span className={styles.textColor}>View Count: </span>
+                      <span className={styles.channelTitle}>{viewCount}</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              </Modal>
+             
             </div>
         );
 };
