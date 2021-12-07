b56cff3b141fa14b538b7833b4b74c18823097ba
iceweasel <johnsjosephk@protonmail.com>
2021-12-07 19:33:19 +0530

    key render issue fixed

--- components/cardwrap/Cardwrap.js
+++ components/cardwrap/Cardwrap.js
@@ -1,30 +1,29 @@
 import Card from "../card/card";
-import Link from 'next/link' ; 
+import Link from 'next/link' ;
 
 import styles from './Cardwrap.module.css'
 
 const Cardwrap = (props) => {
-    const { title , videos = [] , size } = props ; 
-    console.log({videos});
+    const { title , videos = [] , size } = props ;
  return(<section className={styles.container}>
      <h2 className={styles.title}>{title}</h2>
      <div className={styles.cardWrapper}>
-          { 
+          {
            videos.map( (vd, id) => {
-            return ( 
-             <Link href={`/video/${vd.id}`} passHref> 
+            return (
+             <Link  key={vd.id}
+             href={`/video/${vd.id}`} passHref>
                <a href={`/video/${vd.id}`}>
-                <Card 
+                <Card
                 imgUrl={vd.imgUrl}
                 size={size}
-                key={vd.id}
                 id={id}
                 />
-                </a>   
-             </Link>  
-            ) 
+                </a>
+             </Link>
+            )
            } ) }
-    
+
 
      </div>
      </section>)
