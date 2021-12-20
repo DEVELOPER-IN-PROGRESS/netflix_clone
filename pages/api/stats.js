8229df2375b805dec674ce93570a22ea564571a0
iceweasel <johnsjosephk@protonmail.com>
2021-12-20 20:42:00 +0530

    stats api basic structure

---
+++ pages/api/stats.js
@@ -0,0 +1,18 @@
+import  jwt  from "jsonwebtoken";
+export default async function(req, res){
+    if( req.method === 'POST'){
+        try{
+            const token = req.cookies.token ;
+            if(!req.cookies.token){
+            res.status(403).send({ done: 'Error'})
+        }else{
+            const decoded = jwt.verify(token, process.env.HASURA_JWT_SECRET)
+            console.log({decoded})
+         res.send({ msg: 'It Works' , decoded}) ;
+        }
+      }catch(error){
+          console.error('Error Occured /stats' , error );
+          res.status(500).send( { done:false , error : error?.message });
+      }
+    }
+}
