c12f65319ceb62aa1997942c23512875c8a3d6b6
iceweasel <johnsjosephk@protonmail.com>
2021-12-24 19:39:26 +0530

    user stats functions

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,10 +1,11 @@
-import  jwt  from "jsonwebtoken";
+import { verifyToken } from "../../lib/utils";
+
 export default async function(req, res){
     if( req.method === 'POST'){
         try{
             const token = req.cookies.token ;
             if(!req.cookies.token){
-            res.status(403).send({ done: 'Error'})
+            res.status(403).send({})
         }else{
             const decoded = jwt.verify(token, process.env.HASURA_JWT_SECRET)
             console.log({decoded})
