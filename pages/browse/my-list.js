7f8222c867d83766c70aa01f24d9caf111ba4143
iceweasel <johnsjosephk@pm.me>
2022-01-06 23:08:47 +0530

    added server side render to mylist page

--- pages/browse/my-list.js
+++ pages/browse/my-list.js
@@ -1,8 +1,30 @@
 import Head from "next/head";
 import Cardwrap from "../../components/cardwrap/Cardwrap";
 import NavBar from "../../components/nav/navbar";
+import useRedirectUser from "../../utils/redirectUser";
+import { getMyList } from "../../lib/videos";
 
 import styles from "../../styles/myList.module.css";
+
+export async function getServerSideProps(context) {
+  const { userId, token } = await useRedirectUser(context);
+  if (!userId) {
+    return {
+      props: {},
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+  const videos = await getMyList(userId, token);
+
+  return {
+    props: {
+      myListVideos: videos,
+    },
+  };
+}
 
 const MyList = ({ myListVideos }) => {
     return (
